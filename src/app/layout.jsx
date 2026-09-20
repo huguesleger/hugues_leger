2a266d68d152907efafe6f96f0629e40841b6afc
hugues leger <hugueslegerdevweb@gmail.com>
2026-09-20 15:30:04 +0200

    clean code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import LenisProvider from "@/components/LenisProvider";
 import "../scss/main.scss";
 import TransitionOverlay from "@/components/TransitionOverlay";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Portfolio",
@@ -20,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+    <html lang="fr" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
